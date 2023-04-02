a1725a9885ad72aef7a56cd21762d33c3fb7641f
xXBombardXx <128124569+xXBombardXx@users.noreply.github.com>
2023-04-02 18:39:19 -0400

    Commit from TeleportHQ

--- src/views/sponsor-us.js
+++ src/views/sponsor-us.js
@@ -23,29 +23,23 @@ const SponsorUs = (props) => {
       <div className="sponsor-us-banner">
         <h1 className="sponsor-us-text">Sponsor Us</h1>
       </div>
+      <span className="sponsor-us-text01">Why Sponsor?</span>
+      <span className="sponsor-us-text02">
+        <span>- Sponsorship money is tax deductible</span>
+        <br></br>
+        <span>- Advertisement (Shirts, banners, robots, website, etc.)</span>
+        <br></br>
+        <span>
+          - Raise your profile with the next generation of engineers (internship
+          opportunities)
+        </span>
+        <br></br>
+        <span>
+          - Employees who mentor us gain valuable experience and enhance their
+          professional skills
+        </span>
+      </span>
       <div className="sponsor-us-container1">
-        <div className="sponsor-us-container2">
-          <span className="sponsor-us-text01">Why Sponsor?</span>
-          <span className="sponsor-us-text02">
-            <span>- Sponsorship money is tax deductible</span>
-            <br></br>
-            <span>
-              - Advertisement (Shirts, banners, robots, website, etc.)
-            </span>
-            <br></br>
-            <span>
-              - Raise your profile with the next generation of engineers
-              (internship opportunities)
-            </span>
-            <br></br>
-            <span>
-              - Employees who mentor us gain valuable experience and enhance
-              their professional skills
-            </span>
-          </span>
-        </div>
-      </div>
-      <div className="sponsor-us-container3">
         <Link to="/donate" className="sponsor-us-navlink button">
           Donate Money or Items
         </Link>
